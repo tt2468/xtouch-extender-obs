90db28ff37d3af276be06727a7da4d77de597ba2
tt2468 <tt2468@gmail.com>
2024-08-17 19:01:32 -0700

    Use callbacks for midi receive instead of input
    Significantly reduces CPU usage

--- src/main.py
+++ src/main.py
@@ -26,7 +26,6 @@ async def obs_volumeter_callback(event_data):
                     strip.update_volumeter(source["inputLevelsMul"])
                 break
 
-
 # todo identify event to merge all callbacks
 async def obs_slider_callback(event_data):
     for strip in midi.strips:
@@ -58,6 +57,30 @@ async def obs_monitor_callback(event_data):
             strip.update_monitor(event_data)
             break
 
+def on_midi_message(msg, loop):
+    if not msg:
+        return
+
+    b1 = msg[0][0]
+    b2 = msg[0][1]
+    b3 = msg[0][2]
+
+    try:
+        if b1 == 144:
+            strip = midi.strips[b2 % 8]
+            aw = strip.process_button([b2, b3])
+            asyncio.run_coroutine_threadsafe(aw, loop)
+        elif b1 == 176:
+            strip = midi.strips[b2 % 8]
+            aw = strip.process_encoder([b2, b3])
+            asyncio.run_coroutine_threadsafe(aw, loop)
+        else:
+            strip = midi.strips[b1 - 224]
+            aw = strip.process_fader([b1, b3])
+            asyncio.run_coroutine_threadsafe(aw, loop)
+    except:
+        logging.exception('Exception:\n')
+
 async def main():
     global midi
     midi = midi_lib.Device()
@@ -87,32 +110,14 @@ async def main():
     for i in range(MIDI_STRIP_COUNT):
         await midi.create_strip()
 
+    midi.input.set_callback(on_midi_message, asyncio.get_running_loop())
+
     while True:
         current = time.time_ns()
         for strip in midi.strips:
             if strip.fader_busy and current - strip.fader_delta > FADER_TIMEOUT * 1000000000:
-                strip.pos_fader()
-                await asyncio.sleep(0)
-
-        midi_msg = midi.input.get_message()
-        if not midi_msg:
-            await asyncio.sleep(0)
-            continue
-        b1 = midi_msg[0][0]
-        b2 = midi_msg[0][1]
-        b3 = midi_msg[0][2]
-
-        if b1 == 144:
-            strip = midi.strips[b2 % 8]
-            await strip.process_button([b2, b3])
-        elif b1 == 176:
-            strip = midi.strips[b2 % 8]
-            await strip.process_encoder([b2, b3])
-        else:
-            strip = midi.strips[b1 - 224]
-            await strip.process_fader([b1, b3])
-
-        await asyncio.sleep(0)
+                await asyncio.to_thread(strip.pos_fader)
+        await asyncio.sleep(0.05)
 
     await obs.shutdown()
     await midi.clear_strips()
